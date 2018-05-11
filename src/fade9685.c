2e389bb24ea8111c944f2ef3525a7cd9bb98a6a0
Paul Vint <pjvint@gmail.com>
2018-05-11 22:52:47 +0000

    Adding proper options, fixed channel register

--- src/fade9685.c
+++ src/fade9685.c
@@ -49,7 +49,9 @@ void print_usage(char *name) {
 	printf("  -f\tFrequency in Hz (24-1526)\n");
 	printf("  -d\tDuty Cycle (0 - 4095)\n");
 	printf("  -s\tFade speed (Sets delay in ms between steps)\n");
-	
+	printf("  -b\tBus number (default 1)\n");
+	printf("  -a\tAddress (Default 0x42)\n");
+	printf("  -c\tChannel (0 - 15)\n");
 
 } // print_usage
 
@@ -65,20 +67,54 @@ int main(int argc, char **argv) {
 	int c, ret, fd;
 	unsigned int onVal = 0;
 	unsigned int offVal = 0;
+	unsigned int channel = 0;	// Channel register is channel * 4 + 6
+	unsigned int channelReg;
+	unsigned int bus = 1;
+	unsigned int address = 0x40;
 
 	opterr = 0;
-	while ((c = getopt (argc, argv, "hfds")) != -1)
+
+	static struct option long_options[] = 
+	{
+		{ "frequency", required_argument, 0, 'f' },
+		{ "dutycycle", required_argument, 0, 'd' },
+		{ "speed", required_argument, 0, 's' },
+		{ "channel", required_argument, 0, 'c' },
+		{ "bus", required_argument, 0, 'b' },
+		{ "address", required_argument, 0, 'a' },
+		{ "help", 0, 0, 'h' },
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((c = getopt_long (argc, argv, "fdscba", long_options, NULL)) != EOF)
 	{
 		switch (c)
 		{
 			case 'f':  // Frequency
 				frequency = atoi(optarg);
+				if ( ( frequency < _PCA9685_MINFREQ ) || ( frequency > _PCA9685_MAXFREQ ) )
+				{
+					fprintf( stderr, "Frequency must be %d - %d.\n", _PCA9685_MINFREQ, _PCA9685_MAXFREQ );
+					exit( 1 );
+				}
 			case 'd':  // Duty Cycle
 				dutycycle = atoi(optarg);
+				if ( ( dutycycle < 0 ) || ( dutycycle > 4095 ) )
+				{
+					fprintf( stderr, "Duty cycle must be 0 - 4095\n" );
+				}
 			case 's':  // Speed (delay in ms)
 				delay = atoi(optarg);
 			case 'c':  // Channel   TODO: Allow multiple channels
 				channel = atoi(optarg);
+				if ( ( channel < 0 ) || ( channel >= _PCA9685_CHANS ) )
+				{
+					fprintf( stderr, "Channel must be 0 - %d\n", _PCA9685_CHANS - 1 );
+				}
+			case 'b':  // Bus (default 1)
+				bus = atoi( optarg );
+			case 'a':
+				address = atoi( optarg );
 			case 'h':  // help mode
 				print_usage(argv[0]);
 				exit(0);
@@ -124,11 +160,12 @@ int main(int argc, char **argv) {
 	// Set the duty cycle on and off values
 	offVal = 0xFFF * dutycycle;
 
+	channelReg = channel * 4 + _PCA9685_BASEPWMREG;
 	//ret = PCA9685_setPWMVal(fd, addr, channel, onVal, offVal);
-	ret = PCA9685_setPWMVal(fd, addr, 0, 0, 0xef);
+	ret = PCA9685_setPWMVal(fd, addr, channelReg, 0, 4000);
 
 	int val;
-	val = PCA9685_getPWMVal(fd, addr, 0, &onVal, &offVal);
+	val = PCA9685_getPWMVal(fd, addr, channelReg, &onVal, &offVal);
 	fprintf(stderr, "On: %03x   Off: %03x\n", onVal, offVal);
 	
 	return 0;
